deaabf93db12a8d2aab857adc3f16f8b99f65cb4
Jacob Taggart <jacob.taggart@mobileappscompany.us>
2019-07-15 11:49:36 -0400

    done

---
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package com.example.exoplayer;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+
+/**
+ * A fullscreen activity to play audio or video streams.
+ */
+public class PlayerActivity extends AppCompatActivity {
+  private SimpleExoPlayer player;
+  PlayerView playerView;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
+    playerView = findViewById(R.id.video_view);
+  }
+
+  private void initializePlayer() {
+    player = ExoPlayerFactory.newSimpleInstance(
+            new DefaultRenderersFactory(this),
+            new DefaultTrackSelector(), new DefaultLoadControl());
+
+    playerView.setPlayer(player);
+    boolean playWhenReady = true;
+    int currentWindow = 0;
+    int playbackPosition = 0;
+    player.setPlayWhenReady(playWhenReady);
+    player.seekTo(currentWindow, playbackPosition);
+
+    Uri uri = Uri.parse(getString(R.string.media_url_mp3));
+    MediaSource mediaSource = buildMediaSource(uri);
+    player.prepare(mediaSource, true, true);
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+    return new ExtractorMediaSource.Factory(
+            new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+            createMediaSource(uri);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    if (Util.SDK_INT > 23) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT <= 23 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (Util.SDK_INT <= 23) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (Util.SDK_INT > 23) {
+      releasePlayer();
+    }
+  }
+
+}
